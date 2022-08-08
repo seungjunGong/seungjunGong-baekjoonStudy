01ea9b51b7b95bed71f8b24714dde761ede05655
키다리아저씨 <76648361+seungjunGong@users.noreply.github.com>
2022-08-08 20:16:43 +0900

    [Bronze V] Title: ??!, Time: 224 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine();
+        id += "??!";
+        System.out.println(id);
+        scanner.close();
+    }
+}
