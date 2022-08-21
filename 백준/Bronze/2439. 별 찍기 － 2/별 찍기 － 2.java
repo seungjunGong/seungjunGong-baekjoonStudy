35bc879b296919977727350bb0769c889b330176
키다리아저씨 <76648361+seungjunGong@users.noreply.github.com>
2022-08-21 20:47:02 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 500 ms, Memory: 21504 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine()); // 문자열을 입력받음
+        
+        for(int i = 1; i<=n; i++){
+            for(int j=n; j>i; j--){
+                System.out.printf(" ");
+            }
+            for(int k = 1; k<=i; k++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+        bf.close();
+    }
+}
