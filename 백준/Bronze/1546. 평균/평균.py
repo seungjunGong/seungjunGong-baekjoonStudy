5d0f70f38b768d5a93cfe3bdc91940025d19eb8b
키다리아저씨 <jjw5741@naver.com>
2022-09-20 09:59:53 +0900

    [Bronze I] Title: 평균, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,12 +1,8 @@
-scores = list()
-result = 0
-num =int(input(""))
-score = input("")
-scores = score.split(" ")
-for i in range(0,len(scores)):
-    scores[i] = int(scores[i])
-scores.sort()
-max = int(scores[-1])
-for n in scores:
-    result += int(n) / max * 100
-print(result/num)
+N = int(input())
+number = list(map(int, input().split(' ')))
+max_num = max(number)
+
+for i in range(N):
+    number[i] = number[i] / max_num * 100
+
+print(sum(number)/N)
