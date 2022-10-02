c2188293326f0e8bdc919fe37bae05d33444950c
키다리아저씨 <jjw5741@naver.com>
2022-10-02 10:23:37 +0900

    [Silver II] Title: 좌표 압축, Time: 7932 ms, Memory: 145280 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,20 +1,16 @@
 import sys
-N = int(sys.stdin.readline())
-
-xList = list(map(int, sys.stdin.readline().split(' ')))
-xZipList = sorted(list(set(xList)))
+N = int(input())
+xList = list(map(int, input().split(' ')))
+xSort = sorted(list(set(xList)))
 
 for x in xList:
     start = 0
-    end = len(xZipList)
-
-    while start <= end:
-        mid = (start + end) // 2        # 이진탐색 사용 
-
-        if xZipList[mid] == x:
+    end = len(xSort)
+    while 1:
+        mid = (start + end)//2
+        if x == xSort[mid]:
             sys.stdout.write(f"{mid} ")
             break
-        elif xZipList[mid] < x:
+        if x > xSort[mid]:
             start = mid + 1
-        else:
-            end = mid - 1
+        else: end = mid - 1
