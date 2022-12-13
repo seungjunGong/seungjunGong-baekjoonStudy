ca2a2cd20a88422269904ae3a81125d3f91c07e8
키다리아저씨 <jjw5741@naver.com>
2022-12-13 11:04:47 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 4024 ms, Memory: 33640 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,14 +1,21 @@
+'''
+가장 작은 2개의 묶음을 더한 뒤 해당 카드를 버리고
+새로운 묶음을 추가하여 정렬한다. - 우선순위 큐 이용
+위 과정을 n - 1번 반복한다.
+'''
 import heapq
-deckList = []
-sum_deck = 0
-N = int(input())
 
-for _ in range(N):
-    heapq.heappush(deckList, int(input())) 
- 
-for _ in range(N-1):
-    new_deck = heapq.heappop(deckList) + heapq.heappop(deckList)
-    sum_deck += new_deck
-    heapq.heappush(deckList, new_deck)
+n = int(input())
 
-print(sum_deck)
+cards = []
+for i in range(n):
+    card = int(input())
+    heapq.heappush(cards, card)
+
+result = 0
+for _ in range(n-1):
+    new_card = heapq.heappop(cards) + heapq.heappop(cards)
+    result += new_card
+    heapq.heappush(cards, new_card)
+
+print(result)
