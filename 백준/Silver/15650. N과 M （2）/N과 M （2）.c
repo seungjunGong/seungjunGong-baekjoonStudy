8b312ef1afff625e44634bdbc0e8ddd26a401e60
SeungJun Gong <jjw5741@naver.com>
2023-08-29 15:29:12 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int n, m;
+
+void printSequence(int *sequence)
+{
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", sequence[i]);
+    }
+    printf("\n");
+}
+
+void generateSequence(int *sequence, int index, int start)
+{
+    if (index == m)
+    {
+        printSequence(sequence);
+        return;
+    }
+
+    for (int i = start; i <= n; i++)
+    {
+        sequence[index] = i;
+        generateSequence(sequence, index + 1, i + 1);
+    }
+}
+
+int main(void)
+{
+    scanf("%d %d", &n, &m);
+
+    int sequence[m];
+
+    generateSequence(sequence, 0, 1);
+
+    return 0;
+}
