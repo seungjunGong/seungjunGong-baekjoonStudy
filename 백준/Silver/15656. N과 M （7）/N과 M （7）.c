4c12a618755c64ad893d5625b4cad29cc16b8da1
SeungJun Gong <jjw5741@naver.com>
2023-09-01 23:13:05 +0900

    [Silver III] Title: N과 M (7), Time: 748 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int n, m;
+
+void bubble_sort(int *arr)
+{
+    int temp;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printSequence(int *sequence)
+{
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", sequence[i]);
+    }
+    printf("\n");
+}
+
+void generateSequence(int *sequence, int *arr, int index, int start)
+{
+    if (index == m)
+    {
+        printSequence(sequence);
+        return;
+    }
+
+    for (int i = start; i < n; i++)
+    {
+        sequence[index] = arr[i];
+        generateSequence(sequence, arr, index + 1, start);
+    }
+}
+
+int main(void)
+{
+    scanf("%d %d", &n, &m);
+
+    int nArr[n];
+    int sequence[m];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nArr[i]);
+    bubble_sort(nArr);
+
+    generateSequence(sequence, nArr, 0, 0);
+
+    return 0;
+}
