9999fd3bc354cadc008e0d7809bb2571324fcb4b
SeungJun Gong <jjw5741@naver.com>
2023-09-03 11:23:59 +0900

    [Silver II] Title: N과 M (11), Time: 176 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define MAX_SIZE 8
+
+int n, m, check[MAX_SIZE];
+
+void bubble_sort(int *arr)
+{
+    int temp;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printSequence(int *sequence)
+{
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", sequence[i]);
+    }
+    printf("\n");
+}
+
+void generateSequence(int *sequence, int *arr, int index, int start)
+{
+    if (index == m)
+    {
+        printSequence(sequence);
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (tmp != arr[i])
+        {
+            sequence[index] = arr[i];
+            tmp = sequence[index];
+            generateSequence(sequence, arr, index + 1, i);
+        }
+    }
+}
+
+int main(void)
+{
+    scanf("%d %d", &n, &m);
+
+    int nArr[n];
+    int sequence[m];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nArr[i]);
+    bubble_sort(nArr);
+
+    generateSequence(sequence, nArr, 0, 0);
+
+    return 0;
+}
