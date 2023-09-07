07ba59bb59fb10e55f2ebe42c635ad7c2eb11d02
SeungJun Gong <jjw5741@naver.com>
2023-09-07 23:48:16 +0900

    [Silver III] Title: 모든 순열, Time: 28 ms, Memory: 1156 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAX_SIZE 10000
+
+int n, check;
+int s[MAX_SIZE];
+
+int next_permutation()
+{
+    int idx = n - 1;
+    while (idx > 0 && s[idx - 1] > s[idx])
+        idx--;
+
+    if (idx == 0)
+        return 0;
+
+    int idy = n - 1, tmp;
+    while (s[idx - 1] > s[idy])
+        idy--;
+
+    tmp = s[idx - 1];
+    s[idx - 1] = s[idy];
+    s[idy] = tmp;
+
+    idy = n - 1;
+    while (idx < idy)
+    {
+        tmp = s[idx];
+        s[idx] = s[idy];
+        s[idy] = tmp;
+        idx++;
+        idy--;
+    }
+
+    return 1;
+}
+
+int main(void)
+{
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        s[i] = i + 1;
+        printf("%d ", s[i]);
+    }
+    printf("\n");
+
+    check = 0;
+    while (!check)
+    {
+        if (next_permutation(s))
+        {
+            for (int i = 0; i < n; i++)
+                printf("%d ", s[i]);
+            printf("\n");
+        }
+        else
+            check = 1;
+    }
+
+    return 0;
+}
