7777af4561895a42e6570e0daf79ca9619a9d944
SeungJun Gong <jjw5741@naver.com>
2023-10-09 11:58:49 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.00 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = []
+    s = list(s)
+    s_index = {}
+    
+    for i, alp in enumerate(s):
+        if s_index.get(alp) == None:
+            answer.append(-1)
+            s_index[alp] = i
+        else:
+            answer.append(i - s_index[alp])
+            s_index[alp] = i
+                    
+    return answer
