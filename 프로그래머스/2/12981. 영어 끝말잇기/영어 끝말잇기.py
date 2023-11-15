83153b9b3f25e3dd9cf0c871a39ed891df29fb47
SeungJun Gong <jjw5741@naver.com>
2023-11-15 09:16:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -2,15 +2,13 @@ def solution(n, words):
     answer = []
 
     counts = {words[0] : 1}
-    fail_idx = 0
-    turn = 0
+    fail_idx, turn = 0, 0
     
     for i in range(1, len(words)):
         word = words[i]
         counts[word] = counts.get(word, 0) + 1
         
         if words[i-1][-1] != words[i][0] or counts[word] > 1:
-            print(words[i])
             turn = i // n  + 1
             fail_idx = i % n + 1
             break
