ad461c269374e156f69f8b220791006199b9704c
SeungJun Gong <jjw5741@naver.com>
2023-12-01 09:44:12 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,28 +1,26 @@
 '''
-기술 시전 시간
-1초당 회복량
-추가 회복량
-[공격시간, 피해량]
+bandage = [기술 시전 시간, 1초당 회복량, 추가 회복량]
+attacks = [공격시간, 피해량]
 '''
 def solution(bandage, health, attacks):
     answer = 0
-    time = 0
-    band_time = 0
-    mx_hp = health
-    end_time = attacks[-1][0]
+    time = 0                    # 현재 시간
+    band_time = 0               # 붕대 감는 시간
+    hp = health                 # 현재 체력
+    end_time = attacks[-1][0]   # 종료 시간
     
-    while(time < end_time and health > 0):
+    while(time < end_time and hp > 0): # 종료시간, 체력 체크 
         time += 1
-        if time == attacks[0][0]:
-            health -= attacks[0][1]
+        if time == attacks[0][0]:   # 공격 받은 경우
+            hp -= attacks[0][1]
             del attacks[0]
             band_time = 0
-        else:
+        else:                       # 밴드 감는 경우     
             band_time += 1
-            health = min(mx_hp, health + bandage[1])
-            if band_time == bandage[0]:
-                health = min(mx_hp, health + bandage[2])
+            hp = min(health, hp + bandage[1])         # 1초 회복, 최대 체력 보정
+            if band_time == bandage[0]:               # 추가 회복
+                hp = min(health, hp + bandage[2])     # 추가 회복, 체력 보정
                 band_time = 0
     
-    answer = health if health > 0 else -1
+    answer = hp if hp > 0 else -1
     return answer
