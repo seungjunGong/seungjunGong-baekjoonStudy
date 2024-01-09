8bd7901ef57d8f1d76e82e47a6e80078377c7672
Seungjun Gong <jjw5741@naver.com>
2024-01-09 10:07:12 +0900

    [level 2] Title: 프로세스, Time: 0.47 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,33 +2,29 @@ import java.util.*;
 
 class Solution {
     
-    boolean check_priority(List<Integer> list){
-    for(int i = 1; i < list.size(); i++)
-        if(list.get(i) > list.get(0))
-            return false;
-    return true;
-    }
-    
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        
+        int peek = 0;
+            
         List<Integer> queue = new ArrayList<>();
         for(int p : priorities)
             queue.add(p);
         
-        while(true){
-            if(check_priority(queue)){
+        Arrays.sort(priorities); // idea -> 정렬을 수행 했을 때 첫번 째 값만 비교하면 
+                                 // 나머지 값은 전부 우선순위가 작으므로 비교할 필요가 없다.
+        
+        int size = priorities.length - 1;
+        while(!queue.isEmpty()){
+            peek = queue.remove(0);
+            if(peek == priorities[size - answer]){ // 우선순위가 가장 높은 경우
                 answer++;
                 if(location == 0)
                     break;   
-                else
-                    queue.remove(0);                    
-            }else{
-                queue.add(queue.remove(0));
+            }else{                                 // 우선 순위가 높은 것이 있는 경우
+                queue.add(peek);
             }
             location = (location + queue.size() - 1) % queue.size();
         }
-        
         
         return answer;
     }
