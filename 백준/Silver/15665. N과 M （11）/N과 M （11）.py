b93e0967a0443f6645c58b441ca19b02d0f2e409
Seungjun Gong <jjw5741@naver.com>
2024-01-19 16:20:38 +0900

    [Silver II] Title: N과 M (11), Time: 696 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,15 @@
+def solution(sequence, stack, m):
+    if len(stack) == m:
+        print(" ".join(map(str, map(lambda i: sequence[i], stack))))
+        return
+    
+    prev = 0
+    for i in range(len(sequence)):
+        if prev != sequence[i]:
+            stack.append(i)
+            solution(sequence, stack, m)
+            prev = sequence[stack.pop()]
+
+n, m = map(int, input().split())
+sequence = sorted(list(map(int, input().split())))
+solution(sequence, [], m)
