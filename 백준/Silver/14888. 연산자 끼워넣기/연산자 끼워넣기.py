be533e0c135536eb413b1738d45b3f27b511461e
Seungjun Gong <jjw5741@naver.com>
2024-01-22 11:04:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,37 +1,33 @@
 n = int(input())
+numbers = list(map(int, input().split()))
+opers = list(map(int, input().split()))
 
-array = list(map(int, input().split()))
-oper = list(map(int, input().split()))
+max_v = -1000000000
+min_v = 1000000000
 
-min_value = 1e9
-max_value = -1e9
-
-def dfs(i, now):
-    global min_value, max_value
-    if i == n:
-        min_value = min(min_value, now)
-        max_value = max(max_value, now)
+def calc(v, num, oper):
+    if oper == 0:
+        v += num
+    elif oper == 1:
+        v -= num
+    elif oper == 2:
+        v *= num
     else:
-        if oper[0] > 0:
-            oper[0] -= 1
-            # 다음 인덱스, 현재 값 + 다음 값
-            dfs(i + 1, now + array[i])
-            # 해당 재귀가 끝나면 다시 복구
-            oper[0] += 1
-        if oper[1] > 0:
-            oper[1] -= 1
-            dfs(i + 1, now - array[i])
-            oper[1] += 1
-        if oper[2] > 0:
-            oper[2] -= 1
-            dfs(i + 1, now * array[i])
-            oper[2] += 1
-        if oper[3] > 0:
-            oper[3] -= 1
-            dfs(i + 1, int(now / array[i]))
-            oper[3] += 1
+        v = int(v / num)
+    return v
 
-dfs(1, array[0])
+def backtrack(idx, v):
+    global max_v, min_v
+    if idx == len(numbers):
+        max_v = max(max_v, v)
+        min_v = min(min_v, v)
+        return
 
-print(max_value)
-print(min_value)
+    for i in range(4):
+        if opers[i] > 0:
+            opers[i] -= 1
+            backtrack(idx+1, calc(v, numbers[idx], i))
+            opers[i] += 1
+
+backtrack(1, numbers[0])
+print(f"{max_v}\n{min_v}")
