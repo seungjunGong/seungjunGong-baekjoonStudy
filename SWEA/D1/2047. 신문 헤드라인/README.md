e605de76411809a1336bbb425656a6e211d32cf9
Seungjun Gong <jjw5741@naver.com>
2024-10-10 11:27:59 +0900

    [D1] Title: 신문 헤드라인, Time: 143 ms, Memory: 20,668 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/README.md
@@ -0,0 +1,15 @@
+# [D1] 신문 헤드라인 - 2047 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq) 
+
+### 성능 요약
+
+메모리: 20,668 KB, 시간: 143 ms, 코드길이: 346 Bytes
+
+### 제출 일자
+
+2024-10-10 11:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        String s;
+        s=sc.nextLine().toUpperCase();
+
+        System.out.println(s);
+    }
+}
