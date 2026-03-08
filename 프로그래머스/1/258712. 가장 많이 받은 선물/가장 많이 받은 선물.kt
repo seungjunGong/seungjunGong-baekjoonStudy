8cc2dc993433ae1d2b2178702f4e4f3c76830b34
Seungjun Gong <jjw5741@naver.com>
2026-03-08 22:42:42 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 49.54 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
@@ -1,40 +1,45 @@
+// version repactoring
 class Solution {
     fun solution(friends: Array<String>, gifts: Array<String>): Int {
-        val mapping = mutableMapOf<String, Int>()
         val n = friends.size
-        val giftRecords = Array(n) { IntArray(n) }
-        val giftScore = IntArray(n)
-        val nextGiftCounts = IntArray(n)
+        val nameToIndex = mutableMapOf<String, Int>()
+        val giftHistory = Array(n) { IntArray(n) }
+        val giftBalance = IntArray(n) // 준 수 - 받은 수
+        val nextReceivedCount = IntArray(n)
         
         friends.forEachIndexed { index, friend ->
-            mapping[friend] = index
+            nameToIndex[friend] = index
         }
         
         gifts.forEach { gift ->
-            val (a, b) = gift.split(" ")
-            val (giver, receiver) = mapping[a]!! to mapping[b]!!
-            giftRecords[giver][receiver] += 1
-            giftScore[giver] += 1
-            giftScore[receiver] -= 1
+            val (giverName, receiverName) = gift.split(" ")
+            val giver = nameToIndex[giverName]!!
+            val receiver = nameToIndex[receiverName]!!
+            
+            giftHistory[giver][receiver] += 1
+            giftBalance[giver] += 1
+            giftBalance[receiver] -= 1
         }
         
         for (i in 0 until n) {
             for (j in i + 1 until n) {
-                if (giftRecords[i][j] > giftRecords[j][i]) {
-                    nextGiftCounts[i] += 1
-                } else if (giftRecords[i][j] < giftRecords[j][i]) {
-                    nextGiftCounts[j] += 1
+                val iToJ = giftHistory[i][j]
+                val jToI = giftHistory[j][i]
+                
+                if (iToJ > jToI) {
+                    nextReceivedCount[i]++
+                } else if (iToJ < jToI) {
+                    nextReceivedCount[j]++
                 } else {
-                    if (giftScore[i] > giftScore[j]) {
-                        nextGiftCounts[i] += 1
-                    } else if (giftScore[i] < giftScore[j]) {
-                        nextGiftCounts[j] += 1
+                    if (giftBalance[i] > giftBalance[j]) {
+                        nextReceivedCount[i]++
+                    } else if (giftBalance[i] < giftBalance[j]) {
+                        nextReceivedCount[j]++
                     }
                 }
             }
         }
         
-        
-        return nextGiftCounts.maxOrNull()!!
+        return nextReceivedCount.maxOrNull()!!
     }
 }
