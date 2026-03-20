50a946b173dd0ca24a9595480d419a870880cc5a
Seungjun Gong <jjw5741@naver.com>
2026-03-20 11:32:06 +0900

    [Silver IV] Title: 덱 2, Time: 1080 ms, Memory: 262248 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.kt
+++ 백준/Silver/28279. 덱 2/덱 2.kt
@@ -9,17 +9,17 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val sb = StringBuilder()
     repeat(n) {
         val st = StringTokenizer(readLine())
-        val commend = st.nextToken().toInt()
+        val command = st.nextToken().toInt()
         
-        val output = when(commend) {
+        val output = when(command) {
             1 -> { deque.addFirst(st.nextToken().toInt()); null }
             2 -> { deque.addLast(st.nextToken().toInt()); null }
             3 -> if (deque.isEmpty()) -1 else deque.removeFirst()
             4 -> if (deque.isEmpty()) -1 else deque.removeLast()
             5 -> deque.size
             6 -> if (deque.isEmpty()) 1 else 0
-            7 -> if (deque.isEmpty()) -1 else deque.first()
-            8 -> if (deque.isEmpty()) -1 else deque.last()
+            7 -> deque.firstOrNull() ?: -1
+            8 -> deque.lastOrNull() ?: -1
             else -> null
         }
         
