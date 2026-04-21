245194c2a677adecf24a137bc843e5421ef4d7bf
Seungjun Gong <jjw5741@naver.com>
2026-04-21 19:00:53 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 124 ms, Memory: 37120 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -16,16 +16,17 @@ def find_brother():
             print(visited[x])
             return
         
-        move = [2 * x, x + 1, x - 1]
+        move = [2 * x, x - 1, x + 1]
         for i, nx in enumerate(move):
             if 0 <= nx < VISITED_SIZE:
-                # 0초인 경우
-                if i == 0 and (visited[nx] == -1 or visited[nx] > visited[x]):
-                    visited[nx] = visited[x]
-                    queue.appendleft(nx) # 우선 탐색
-                elif visited[nx] == -1:
-                    visited[nx] = visited[x] + 1
-                    queue.append(nx)
+                if visited[nx] == -1:
+                    # 0초인 경우
+                    if i == 0:
+                        visited[nx] = visited[x]
+                        queue.appendleft(nx) # 우선 탐색
+                    else:
+                        visited[nx] = visited[x] + 1
+                        queue.append(nx)
 
 if N >= K:
     time = N - K
