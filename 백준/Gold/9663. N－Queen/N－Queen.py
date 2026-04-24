941fecb99c0c4af509d2ee73afa590492791acac
Seungjun Gong <jjw5741@naver.com>
2026-04-24 11:08:40 +0900

    [Gold IV] Title: N-Queen, Time: 20140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,24 +1,27 @@
-ans = 0
+N = int(input())
 
-def promising(row, colList):
-    for i in range(row):
-        if (colList[row] == colList[i]  # 열 체크
-            or abs(colList[row] - colList[i]) == abs(row-i)):   # 대각선 체크
-            return False
-    return True
+visited1 = [False for _ in range(N)]
+visited2 = [False for _ in range(2*N-1)]
+visited3 = [False for _ in range(2*N-1)]
 
-def n_queens(n, row, colList):
-    global ans
-    if row == n:
-        ans += 1
+cnt = 0
+def solution(row):
+    global cnt
+    if row == N:
+        cnt += 1
         return
-    
-    for i in range(n):
-        colList[row] = i
-        if promising(row, colList):
-            n_queens(n, row+1, colList)
-
-n = int(input())
-colList = [0] * n
-n_queens(n, 0, colList)
-print(ans)
+    for col in range(N):
+        if visited1[col] or visited2[col+row] or visited3[row-col+N-1]:
+            continue
+        visited1[col] = True # 열 방문
+        visited2[col+row] = True # 왼쪽 대각선 방문
+        visited3[row-col+N-1] = True # 오른쪽 대각선 방문
+        
+        solution(row+1) # 다음행 방문
+        # 방문 초기화
+        visited1[col] = False
+        visited2[col+row] = False
+        visited3[row-col+N-1] = False
+        
+solution(0)
+print(cnt)
