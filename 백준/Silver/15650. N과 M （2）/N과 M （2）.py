dd3f772b4e2851688a1c26964c45fd2ba432ac5b
Seungjun Gong <jjw5741@naver.com>
2026-04-28 13:52:20 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,26 +1,13 @@
-def solution(stack, n, m):
-    if len(stack) == m:
-        print(" ".join(map(str, stack)))
-        return
-    for i in range(1, n+1):
-        if not stack or stack[-1] < i:
-            stack.append(i)
-            solution(stack, n, m)
-            stack.pop()
+N, M = map(int, input().split())
+s = [0 for _ in range(M)]
 
-'''
-배열 이용 풀이
-def solution(stack, start, index, n, m):
-    if index == m:
-        print(" ".join(map(str, stack)))
+def solution(k, start):
+    if k == M:
+        print(" ".join(map(str, s)))
         return
-    for i in range(start, n+1):
-        stack[index] = i
-        solution(stack, i+1, index+1, n, m)
-n, m = map(int, input().split())
-stack = [0 for _ in range(m)]
-solution(stack, 1, 0, n, m)
-'''
-
-n, m = map(int, input().split())
-solution([], n, m)
+    
+    for i in range(start, N+1):
+        s[k] = i
+        solution(k+1, i+1)
+        
+solution(0, 1)
