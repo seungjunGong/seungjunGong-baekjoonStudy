5208ec418c2f8b7981ad63904e41079dfc8df2db
Seungjun Gong <jjw5741@naver.com>
2026-04-28 13:58:18 +0900

    [Silver III] Title: N과 M (3), Time: 1112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,11 +1,12 @@
-def solution(stack, n, m):
-    if len(stack) == m:
-        print(" ".join(map(str, stack)))
-        return
+N, M = map(int, input().split())
+s = [0 for _ in range(M)]
 
-    for i in range(1, n+1):
-        stack.append(i)
-        solution(stack, n, m)
-        stack.pop()
-n, m = map(int, input().split())
-solution([], n, m)
+def solution(k):
+    if k == M:
+        print(" ".join(map(str, s)))
+        return 
+    
+    for i in range(1, N+1):
+        s[k] = i
+        solution(k+1)
+solution(0)
