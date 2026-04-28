564254e24c7c52d467909326a2f63bc8308f0169
Seungjun Gong <jjw5741@naver.com>
2026-04-28 15:34:02 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,14 +1,13 @@
-def solution(sequence, stack, m):
-    if len(stack) == m:
-        print(" ".join(map(str, stack)))
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+s = [0 for _ in range(M)]
+
+def solution(k, start):
+    if k == M:
+        print(" ".join(map(str, s)))
         return
     
-    for i in sequence:
-        if not stack or stack[-1] < i:
-            stack.append(i)
-            solution(sequence, stack, m)
-            stack.pop()
-            
-n, m = map(int, input().split())
-sequence = sorted(list(map(int, input().split())))
-solution(sequence, [], m)
+    for i in range(start, N):
+        s[k] = arr[i]
+        solution(k+1, i+1)
+solution(0, 0)
