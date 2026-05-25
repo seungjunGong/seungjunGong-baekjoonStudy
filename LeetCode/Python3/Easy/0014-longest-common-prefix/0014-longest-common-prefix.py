a7a58bc855cb33e63a596859f24a71f352ea59b6
Seungjun Gong <jjw5741@naver.com>
2026-05-25 21:56:04 +0900

    Time: 0 ms (100%), Space: 19.4 MB (31.96%) - LeetHub

--- LeetCode/Python3/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
+++ LeetCode/Python3/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -6,15 +6,11 @@ class Solution:
         for s in strs:
             min_length = len(s) if len(s) < min_length else min_length
         
-        prefix_list = []
-        
         for i in range(min_length):
             common = strs[0][i]
 
             for s in strs:
                 if s[i] != common:
-                    return "".join(prefix_list)
+                    return strs[0][:i]
             
-            prefix_list.append(common)
-
-        return "".join(prefix_list)
+        return strs[0][:min_length]
