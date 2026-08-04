6c2967e568b6a89f435d70e8d27ff7a31b53e34a
Seungjun Gong <jjw5741@naver.com>
2026-08-04 20:40:53 +0900

    Time: 379 ms (61.06%), Space: 19.7 MB (6.46%) - LeetHub

---
+++ LeetCode/Python3/Hard/0282-expression-add-operators/0282-expression-add-operators.py
@@ -0,0 +1,36 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        answer = []
+        length = len(num)
+
+        def dfs(idx, express, total, prev):
+            if idx == length:
+                if total == target:
+                    answer.append(express)
+                return
+
+            for i in range(idx, length):
+                number_str = num[idx:i + 1] # 남은 숫자
+
+                if len(number_str) > 1 and number_str[0] == "0":
+                    break
+
+                number = int(number_str)
+
+                if idx == 0:
+                    # 첫 숫자
+                    dfs(i + 1, number_str, number, number)
+                else:
+                    # 더하기
+                    plus_total = total + number
+                    dfs(i + 1, express + "+" + number_str, plus_total, number)
+                    # 빼기
+                    minus_total = total - number
+                    dfs(i + 1, express + "-" + number_str, minus_total, -number)
+                    # 곱하기
+                    multi_total = total - prev + prev * number
+                    dfs(i + 1, express + "*" + number_str, multi_total, prev * number)
+
+        dfs(0, "", 0, 0)
+
+        return answer
